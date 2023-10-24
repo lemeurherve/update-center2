c459039a2b756e2b195b043ce5268fe90d9bccd3
smerle33 <stephane.merle+jenkins@gmail.com>
2023-10-24 10:35:21 +0200

    wip parallel

--- site/publish.sh
+++ site/publish.sh
@@ -63,6 +63,7 @@ echo '--------------------------- Launch Parallelization -----------------------
 # (time aws s3 sync "${ROOT_FOLDER}"/www3/ s3://"${UPDATES_R2_BUCKETS}"/ --profile updates-jenkins-io --no-progress --no-follow-symlinks --size-only --endpoint-url "${UPDATES_R2_ENDPOINT}") 1>"${ROOT_FOLDER}"/output-awsS3.log 2>&1 &
 # wait
 
+rm -f "${ROOT_FOLDER}"/output-pkgcopy.log "${ROOT_FOLDER}"/output-azcopy.log "${ROOT_FOLDER}"/output-awsS3.log
 
 rsync_to_pkg() {
     time rsync -acz "${ROOT_FOLDER}"/www2/ --exclude=/updates --delete --stats ${RSYNC_USER}@${UPDATES_SITE}:/tmp/lemeurherve/pr-745/www/${UPDATES_SITE} 1>"${ROOT_FOLDER}"/output-pkgcopy.log 2>&1
@@ -77,8 +78,11 @@ aws_s3_sync_to_cloudfare() {
 }
 export -f aws_s3_sync_to_cloudfare
 
-parallel rsync_to_pkg azcopy_to_fileshare aws_s3_sync_to_cloudfare ::: 1
-
+parallel --halt-on-error now,fail=1 \
+    rsync_to_pkg \
+    azcopy_to_fileshare \
+    aws_s3_sync_to_cloudfare \
+    ::: 1
 
 # wait for all deferred task
 echo '===============================    all done   ============================'
@@ -88,8 +92,6 @@ echo '-------------------------------     azcopy    ----------------------------
 cat "${ROOT_FOLDER}"/output-azcopy.log
 echo '-------------------------------     aws S3    ----------------------------'
 cat "${ROOT_FOLDER}"/output-awsS3.log
-
-rm "${ROOT_FOLDER}"/output-pkgcopy.log "${ROOT_FOLDER}"/output-azcopy.log "${ROOT_FOLDER}"/output-awsS3.log
 
 ## TODO: test if needed rclone both rsync VM and R2 bucket(s) replacing these 2 calls
 
