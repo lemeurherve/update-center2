56118c09a2fe4c1b4785541a57593db1da5a1920
Hervé Le Meur <lemeurherve.jenkins@gmail.com>
2024-03-07 20:04:23 +0100

    fixup: mirrorbits pod name doesn't contains "-lite" anymore

--- site/publish.sh
+++ site/publish.sh
@@ -142,6 +142,6 @@ then
     mirrorbits_namespace='updates-jenkins-io'
 
     # Requires a valid kubernetes credential file at $KUBECONFIG or $HOME/.kube/config by default
-    pod_name=$(kubectl --namespace="${mirrorbits_namespace}" --no-headers=true get pod --output=name | grep mirrorbits-lite | head -n1)
+    pod_name=$(kubectl --namespace="${mirrorbits_namespace}" --no-headers=true get pod --output=name | grep mirrorbits | head -n1)
     kubectl --namespace="${mirrorbits_namespace}" --container=mirrorbits-lite exec "${pod_name}" -- mirrorbits scan -all -enable -timeout=120
 fi
